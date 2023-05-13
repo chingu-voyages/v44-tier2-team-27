a3ab369b62c213d88a059c75acab3f267f73e2ae
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-13 03:18:27 +0000

    Generated Random Bots
    Declared a function that generates random bots based on the number passed to it.

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import {useBots} from "./context/botsContext.tsx";
 import  BattlePage  from "./components/BattlePage";
 import { WelcomePage } from "./components/WelcomePage";
 import { Layout } from "./components/layout/Layout";
 import { ConfigurationPanel } from "./components/ConfigurationPanel";
+import {generateRandomBot} from "./misc/functions.ts";
 
 function App() {
   const [mainComponent, setMainComponent] = useState("welcomePage");
+  const { createNewBot } = useBots();
+
+  /* the useEffect runs twice because of strict mode.
+   You can disable it temporary so that it generates the exact amount of
+   bots passed. But it doesn't happen in production, so we can turn it on
+   when building
+   */
+  useEffect(() => {
+  for (let i = 0; i < 4; i++) {
+    createNewBot(generateRandomBot(4));
+  }
+  }, [])
 
   const navigateToConfigurationPanel = () => {
     setMainComponent("configurationPanel");
--- src/misc/functions.ts
+++ src/misc/functions.ts
@@ -1,5 +1,16 @@
 //all globally used functions will be defined and exported from here
 
+import {Direction, Operator} from "./interfaces.ts";
+
 export const getRandomNumber = (min: number, max: number): number => {
   return Math.round(Math.random() * (max - min) + min);
 };
+
+export const generateRandomBot = (n: number) =>  {
+  const name = `Bot ${Math.floor(Math.random() * n) + 1}`;
+  const operator = ['AND', 'OR', 'NOR', 'NOT'][Math.floor(Math.random() * 4)] as Operator;
+  const value: 0 | 1 = Math.floor(Math.random() * 2) as 0 | 1;
+  const speed: number = Math.floor(Math.random() * 5) + 1; // between 1 and 5
+  const direction = ['North', 'South', 'East', 'West'][Math.floor(Math.random() * 4)] as Direction;
+  return { name, operator, value, speed, direction };
+}
