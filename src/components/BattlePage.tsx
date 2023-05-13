677d2e1c6905076eb8d058061be5eeee1648978c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-13 03:24:25 +0000

    Rendered Bots

--- src/components/BattlePage.tsx
+++ src/components/BattlePage.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import "../styles/components/board.css";
 import Grid from "./Grid";
-import Bot from "../classes/bot";
+import { BotComponent } from "./Bot.tsx";
+import { useBots } from "../context/botsContext.tsx";
 
 const BattlePage: React.FC = () => {
-  const botsArray: Bot[] = [];
-
-  for (let i = 0; i < 4; i++) {
-    const bot = new Bot(`Bot ${i + 1}`, "AND", 0, 5, "North");
-    botsArray.push(bot);
-  }
+  const { bots } = useBots();
+  console.log(bots);
   const BotDetails: React.FC = () => {
     return (
       <div className="bot-details-container">
-        {botsArray.map((bot, i) => (
+        {bots.map((bot, i) => (
           <div className="bot-details" key={i}>
             <p className="bot-name">{bot.name}</p>
             <div className="score"></div>
@@ -33,7 +30,20 @@ const BattlePage: React.FC = () => {
     <div className="board-container">
       <h1 className="board-heading">LeaderBoard</h1>
       <BotDetails />
-      <Grid />
+      <Grid
+        rows={8}
+        cols={8}
+        botRenderer={(row, col) => {
+          const bot = bots.find(
+            (bot) => bot.position.x === row && bot.position.y === col
+          );
+          if (bot) {
+            return <BotComponent bot={bot} />;
+          } else {
+            return null;
+          }
+        }}
+      />
       <button type="button" className="battle-button">
         BATTLE
       </button>
