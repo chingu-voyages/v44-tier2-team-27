e78f8f20dfde27cbdc253fd17add62c1683a7a11
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-13 03:23:43 +0000

    REFACTOR: Refactored code to add bot props

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../styles/components/grid.css';
 
-const Grid: React.FC = () => {
-  const rows = 8;
-  const cols = 8;
+interface GridProps {
+  rows: number;
+  cols: number;
+  botRenderer: (row: number, col: number) => React.ReactNode;
+}
+
+const Grid: React.FC<GridProps> = ({ rows, cols, botRenderer }: GridProps) => {
   const grid = [];
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      grid.push(<div key={`${i}-${j}`} id={`${i + 1},${j + 1}`} className="grid-cell" />);
+      grid.push(
+          <div key={`${i}-${j}`} id={`${i + 1},${j + 1}`} className="grid-cell">
+            {botRenderer(i, j)}
+          </div>
+      );
     }
   }
 
