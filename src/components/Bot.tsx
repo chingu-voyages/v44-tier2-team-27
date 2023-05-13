d84222f42b740dc4c8e1a0588df4f0d3782a41c6
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-13 03:21:22 +0000

    ADD: Bot component. For now im just rendering name of bot

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1,5 +1,11 @@
-export const Bot = () => {
-	return (
-		<></>
-	)
+import React from "react";
+import Bot from "../classes/bot.ts";
+interface BotComponentProps {
+  bot: Bot;
 }
+
+export const BotComponent: React.FC<BotComponentProps> = ({
+  bot,
+}: BotComponentProps) => {
+  return <div>{bot.name}</div>;
+};
