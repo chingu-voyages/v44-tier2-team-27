ad72d2d59edc97e149af01e94b240398a3c604b0
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2023-05-21 12:47:41 +0100

    feat: add instruction modal that links from footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
+import Modal from './Modal';
+
 export const Footer = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="footerWrapper">
-        <a href="https://github.com/chingu-voyages/v44-tier2-team-27">
-          <p>Chingu 2023</p>
-          <p>voyage: 44</p>
-          <p>team: 27</p>
-        </a>
+      <a href="https://github.com/chingu-voyages/v44-tier2-team-27">
+        <p>Chingu 2023</p>
+        <p>voyage: 44</p>
+        <p>team: 27</p>
+      </a>
 
-        {/* Instruction modal goes here */}
-        <a className="instructionModal "href="#">
-          <h4>how to play</h4>
-          </a>
-          
+      {/* Instruction modal */}
+      <a className="instructionModal" href="#" onClick={handleModalOpen}>
+        <h4>how to play</h4>
+      </a>
+
+      <Modal isOpen={isModalOpen} onClose={handleModalClose} />
     </div>
   );
 };
---
+++ src/components/layout/Modal.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../styles/components/Modal.css'
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-modal" onClick={onClose}>
+          Close
+        </button>
+        <h2>How to Play Boolebots</h2>
+        <p>These instructions will be accessible from the footer anywhere in the app.</p>
+        <ol>
+            <li>First you do this thing</li>
+            <li>Then you do some of this</li>
+            <li>After that, you can do this</li>
+            <li>Finally, you'll want to do this</li>
+            <li>Don't forget to do this</li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
