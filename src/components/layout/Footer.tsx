87039c695ab9adb363755d48063de6e9caa63906
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-21 18:45:08 +0000

    Fixed

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,31 +2,30 @@ import { useState } from 'react';
 import Modal from './Modal';
 
 export const Footer = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const handleModalOpen = () => {
+		setIsModalOpen(true);
+	};
 
-  const handleModalOpen = () => {
-    setIsModalOpen(true);
-  };
+	const handleModalClose = () => {
+		setIsModalOpen(false);
+	};
 
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
+	return (
+		<div className="footerWrapper">
+			<a href="https://github.com/chingu-voyages/v44-tier2-team-27">
+				<p>Chingu 2023</p>
+				<p>voyage: 44</p>
+				<p>team: 27</p>
+			</a>
 
-  return (
-    <div className="footerWrapper">
-      <a href="https://github.com/chingu-voyages/v44-tier2-team-27">
-        <p>Chingu 2023</p>
-        <p>voyage: 44</p>
-        <p>team: 27</p>
-      </a>
+			{/* Instruction modal */}
+			<a className="instructionModal" href="#" onClick={handleModalOpen}>
+				<h4>how to play</h4>
+			</a>
 
-      {/* Instruction modal */}
-      <a className="instructionModal" href="#" onClick={handleModalOpen}>
-        <h4>how to play</h4>
-      </a>
-
-      <Modal isOpen={isModalOpen} onClose={handleModalClose} />
-    </div>
+			<Modal isOpen={isModalOpen} onClose={handleModalClose} />
+		</div>
 	);
 };
--- src/components/layout/Modal.tsx
+++ src/components/layout/Modal.tsx
@@ -1,32 +1,35 @@
 //import React from "react";
-import '../../styles/components/Modal.css'
+import '../../styles/components/Modal.css';
 
 interface ModalProps {
-  isOpen: boolean;
-  onClose: () => void;
+	isOpen: boolean;
+	onClose: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+const Modal = ({ isOpen, onClose }: ModalProps) => {
+	if (!isOpen) return null;
 
-  return (
-    <div className="modal-overlay">
-      <div className="modal-content">
-        <button className="close-modal" onClick={onClose}>
-          Close
-        </button>
-        <h2>How to Play Boolebots</h2>
-        <p>These instructions will be accessible from the footer anywhere in the app.</p>
-        <ol>
-            <li>First you do this thing</li>
-            <li>Then you do some of this</li>
-            <li>After that, you can do this</li>
-            <li>Finally, you will want to do this</li>
-            <li>Do not forget to do this</li>
-        </ol>
-      </div>
-    </div>
-  );
+	return (
+		<div className="modal-overlay">
+			<div className="modal-content">
+				<button className="close-modal" onClick={onClose}>
+					Close
+				</button>
+				<h2>How to Play Boolebots</h2>
+				<p>
+					These instructions will be accessible from the footer anywhere in the
+					app.
+				</p>
+				<ol>
+					<li>First you do this thing</li>
+					<li>Then you do some of this</li>
+					<li>After that, you can do this</li>
+					<li>Finally, you will want to do this</li>
+					<li>Do not forget to do this</li>
+				</ol>
+			</div>
+		</div>
+	);
 };
 
 export default Modal;
