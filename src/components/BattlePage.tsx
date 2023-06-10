0193bad5c82cb2f866a5dcd1f92b66d25737ea9c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-10 16:14:30 +0000

    Quick fixes

--- src/components/BattlePage.tsx
+++ src/components/BattlePage.tsx
@@ -18,7 +18,7 @@ const BattlePage = ({navigateToConfigurationPanel, setDisplayedModal, setIsModal
 	const { bots, editBot, updateBattleLog, setBotWinner, resetBots, botWinner } = useBots();
 	const [play, setPlay] = useState(false);
 	const [timeElapsed, setTimeElapsed] = useState<number>(0);
-	const activeBots = bots.filter((bot) => bot.isAlive);
+	const activeBots = bots.filter((bot) => bot.isActive);
 
 	const handlePlay = () => {
 		setPlay(!play);
